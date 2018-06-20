02e1e38ff0ebd7f54dc52e3c5c6ec3b404a57d21
PaulEibensteiner <paul.eibi@gmx.at>
2018-06-20 15:05:51 -0600

    log finish properly

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,6 @@ var purify = require('gulp-purifycss');
 gulp.task('css', function() {
   return gulp.src('static/css/*.css')
     .pipe(purify(['static/js/*.js', 'layouts/**/*.html']))
-    .pipe(gulp.dest('static/mincss/'));
-  console.log('css purified');
+    .pipe(gulp.dest('static/mincss/'))
+    .on('end', function(){ console.log('CSS purified!'); });
 });
